3346a2e29d73bab9961cefab84c65f00d7855422
raz421 <maharazbhuiyan3@gmail.com>
2025-05-03 17:51:06 +0600

    crate component

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,19 @@
+function Profile() {
+  return (
+    <>
+      <h1>Albert Einstine</h1>
+      <img
+        src="https://theodisseanexperience.com/wp-content/uploads/2021/10/the-odissean-experience-mind-blog-01-5.jpeg"
+        alt="einsstine"
+      />
+    </>
+  );
+}
+export default function Gallery() {
+  return (
+    <>
+      <h1>there are so many scientist</h1>
+      <Profile />
+    </>
+  );
+}
