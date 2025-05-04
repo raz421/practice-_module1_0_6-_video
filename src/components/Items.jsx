b003dbc10b7ff3b2aeec3773bc5b4bff8f3020bb
raz421 <maharazbhuiyan3@gmail.com>
2025-05-04 18:14:41 +0600

    2nd example of coniditional rendering

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,9 +1,19 @@
 export default function Items({ name, ispacked }) {
-  if (ispacked) {
-    return <li className="text-xl font-black ">{name} ✅</li>;
-  }
-  return <li className="text-xl font-black ">{name} ❌</li>;
+  //   if (ispacked) {
+  //     return <li className="text-xl font-black ">{name} ✅</li>;
+  //   }
+  //   return <li className="text-xl font-black ">{name} ❌</li>;
 
   //   here are repeatative code spotted.li tag write twice  with same class,wich is not good
   // the rule of Programminng DRY(Don't Repeat yourself)
+  //   here the soluation
+  return (
+    <div>
+      <li className="text-xl font-black ">
+        {ispacked ? name + "✅" : name + "❌"}
+      </li>
+    </div>
+
+    //there are another problem arise
+  );
 }
